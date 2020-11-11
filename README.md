bfefaab5760ff3242106bd4a001f72a97db17954
Gilson Ponce Briones <gjponce@espol.edu.ec>
2020-11-10 20:34:54 -0500

    modificacion del enum adicionales a clase abstract

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # TallerSOLID
+###### ENUM ADICIONALES
+:eyeglasses: El ENUM puede que este muy desacoplado, ya que si queremos trabajar
+
+en cada uno de los tipos de aderezo que existe, no se podra y no permitira el optimo 
+
+acoplamiento y cohesion a futuras mejoras para su escalabilidad. Puede que en un aderezo
+
+tenga atributos especificos que queremos manejar.
+
--- src/Adicionales/Aderezo.java
+++ src/Adicionales/Aderezo.java
@@ -9,7 +9,22 @@ package Adicionales;
  *
  * @author Pedro Mendoza
  */
-public enum Aderezo {
+/*public enum Aderezo {
     MALVAVISCOS,FRUTILLA,CREMA
     
-}
+}*/
+
+public abstract class Aderezo{
+    private String nombre;
+
+    public Aderezo(String nombre){
+        this.nombre = nombre; 
+    }
+    
+    public abstract void setNombre();
+
+    public String toString() {
+        return nombre.toUpperCase();
+    }
+
+} 
